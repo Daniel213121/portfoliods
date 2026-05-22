fd2d9db804ae202261b8e077eac77489714bcc21
Daniel213121 <danielsackitey10@gmail.com>
2026-05-22 10:56:59 +0000

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next'
 import { Toaster } from 'sonner'
 import './globals.css'
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         {children}
+        <Analytics />
         <Toaster position="bottom-right" richColors closeButton />
       </body>
     </html>
